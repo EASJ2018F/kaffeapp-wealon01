ea2f168026abd6f89570139af7cc4ec9bb3a75da
Hassan Ali Mohdi <hm98-@hotmail.dk>
2018-01-30 12:54:17 +0100

    implementeren opgaven  done

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+    public class FlatWhite: Kaffe
+    {
+
+        public FlatWhite()
+        {
+            Console.WriteLine("FlatWhite");
+        }
+
+
+        public override int Pris()
+        {
+
+
+            return 45;
+        }
+
+        public int MiMælk()
+        {
+            return 160;
+        }
+
+
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+
+
+
+    }
+}
